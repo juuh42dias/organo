4bd2e78905343368e5092ded66597d6fdb1c4728
Juliana Dias <hello@juliana.dev>
2024-10-19 17:45:45 -0300

    Add button, suspend list, and team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+        <Time nome="Programacao"/>
+        <Time nome="Frontend"/>
+        <Time nome="Data Science"/>
     </div>
   );
 }
