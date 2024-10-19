86d7e643b540fbe977705e0fd0319b1a31ce2b36
Juliana Dias <hello@juliana.dev>
2024-10-19 19:51:44 -0300

    Add some adjustments refactoring, but missing footer page

--- src/components/ListaSuspensa/index.js
+++ src/components/ListaSuspensa/index.js
@@ -9,6 +9,7 @@ const ListaSuspensa = (props) => {
         onChange={(evento) => props.aoAlterado(evento.target.value)}
         value={props.valor}
       >
+        <option key="" value=""></option>
         {props.itens.map((item) => (
           <option key={item}>{item}</option>
         ))}
